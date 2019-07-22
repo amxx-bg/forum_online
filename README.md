f0bb195398e5045af372177671e5074172ac4118
Stf Kolev <inkyzfx@gmail.com>
2019-07-22 12:54:15 +0300

    Updated readme and added ext.php

---
+++ README.md
@@ -0,0 +1,11 @@
+# Forum Online
+
+## Installation
+
+Copy the extension to phpBB/ext/evilsystem/forum_online
+
+Go to "ACP" > "Customise" > "Extensions" and enable the "Server Monitoring" extension.
+
+## License
+
+[GPLv2](license.txt)
---
+++ ext.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ *
+ * Forum Online. An extension for the phpBB Forum Software package.
+ *
+ * @copyright (c) 2019, Evil
+ * @license GNU General Public License, version 2 (GPL-2.0)
+ *
+ */
+
+namespace evilsystem\forum_online;
+
+/**
+ * Forum Online Extension base
+ *
+ * It is recommended to remove this file from
+ * an extension if it is not going to be used.
+ */
+class ext extends \phpbb\extension\base
+{
+}
